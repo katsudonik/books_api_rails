2b6ef65fea1140a574f44335d705accf6d277eaf
kaikeda <kaikeda@ikedakatsuyukinoiMac.local>
2021-05-02 03:42:44 +0900

    #6: image belongs to user book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_one :image, dependent: :destroy
+  accepts_nested_attributes_for :image
+  validates_associated :image
 
   validates :title, presence: true, length: { maximum: 100 } 
   validates :body, presence: true, length: { maximum: 255 }
